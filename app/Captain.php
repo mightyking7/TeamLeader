a3e6d2f8a0a50c96577164e9244a973a9509e41b
Isaac buitrago <>
2018-07-14 18:10:45 -0500

    Set fillable and hidden attributes on the class

--- app/Captain.php
+++ app/Captain.php
@@ -2,9 +2,27 @@
 
 namespace App;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 class Captain extends Model
 {
     //
+
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'password'];
+
+
+    /**
+     * The attributes that should be hidden for arrays
+     *
+     * @var array
+     */
+
+    protected $hidden = ['password', 'remember_token'];
+    
 }

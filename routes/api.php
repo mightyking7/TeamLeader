573c1c3d72fd6901fc5e385dd3a4835634b6a325
Isaac buitrago <>
2018-07-21 00:49:13 -0500

    Added create team endpoint to api and included pricing on the home screen

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Team;
 use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +19,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+/**
+ * Business email routes
+ */
 Route::post('/send', 'AccountManager\BusinessEmailController@sendVerificationEmail');
 
 Route::post('/confirm', 'AccountManager\BusinessEmailController@verifyCaptainEmail');
 
+/**
+ * Team routes
+ */
 
+Route::post('/team', 'TeamManager\TeamController@createTeam');
